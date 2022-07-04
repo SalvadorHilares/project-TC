68f870b06f34cafe0a46fca5b7b79f36f95557a7
Salvador Hilares <salvador.hilares@utec.edu.pe>
2022-07-04 04:09:46 -0500

    Version Final

--- Lenguaje Vacio/main.cpp
+++ Lenguaje Vacio/main.cpp
@@ -1,5 +1,6 @@
 #include "forward.h"
 #include "queue.h"
+#include <string.h>
 using namespace std;
 
 struct GIC{
@@ -34,7 +35,7 @@ GIC* create_GIC(){
     return gic;
 }
 
-bool comprobate_terminal(string generator, string s){
+bool comprobate(string generator, string s){
     int n = 0;
     for(int i=0; i<s.size(); i++)
         for(int j=0; j<generator.size(); j++)
@@ -58,7 +59,7 @@ string language_empty(GIC* gic){
     while(true){
         int n = generator.size();
         for(int j=0; j<derecha->size(); j++){
-            if(comprobate_terminal(generator,(*derecha)[j]) && !same(generator,gic->izquierda[0][j]))
+            if(comprobate(generator,(*derecha)[j]) && !same(generator,gic->izquierda[0][j]))
                 generator = generator + gic->izquierda[0][j];
         }
         if (n == generator.size())
@@ -69,14 +70,83 @@ string language_empty(GIC* gic){
     return "NO";
 }
 
-string language_empty_queue(GIC* gic){
-    
+char** create_matriz(GIC* gic){
+    char** matriz = new char*[gic->variables.size()];
+    for(int i=0; i<gic->variables.size(); i++)
+        matriz[i] = new char[2];
+    return matriz;
+}
+
+char** insert_matriz(GIC* gic){
+    char** matriz = create_matriz(gic);
+    for(int i=0; i<gic->variables.size(); i++){
+        matriz[i][0] = gic->variables[i];
+        matriz[i][1] = 'N';
+    }
+    return matriz;
+}
+
+Queue<char>* insert_queue(GIC* gic, char** matriz){
+    Queue<char>* queue = new Queue<char>();
+    string generator = gic->terminales;
+    ForwardList<string>* derecha = gic->derecha;
+    for(int j=0; j<derecha->size(); j++)
+        if(comprobate(generator,(*derecha)[j]) && !same(generator,gic->izquierda[0][j])){
+            queue->enqueue(gic->izquierda[0][j]);
+            for(int k=0; k<gic->variables.size(); k++)
+                if(gic->izquierda[0][j] == matriz[k][0])
+                    matriz[k][1] = 'S';
+        }
+    return queue;
+}
+
+ForwardList<int>* insert_quantity(GIC* gic){
+    ForwardList<int>* quantitys = new ForwardList<int>();
+    for(int i=0; i<gic->derecha->size(); i++){
+        int n = 0;
+        for(int j=0; j<gic->variables.size(); j++){
+            for(int k=0; k<gic->derecha[0][i].size(); k++){
+                if(gic->derecha[0][i][k] == gic->variables[j])
+                    n++;
+            }
+        }
+        quantitys->push_back(n);
+        n=0;
+    }
+    return quantitys;
+}
+
+string language_empty_queue(GIC* gic, char** matriz, Queue<char>* queue, ForwardList<int>* quantity){
+    ForwardList<string>* derecha = gic->derecha;
+    while (!queue->is_empty()){
+        char s = queue->dequeue();
+        const char* aux = &s;
+        for(int i=0; i<derecha->size(); i++){
+            const char* r = (*derecha)[i].c_str();
+            if (strstr(r,aux))
+                (*quantity)[i]--;
+            if((*quantity)[i] == 0){
+                for(int j=0; j<gic->variables.size(); j++)
+                    if(gic->variables[j] == gic->izquierda[0][i] && matriz[j][1] != 'S'){
+                        matriz[j][1] = 'S';
+                        queue->enqueue(gic->izquierda[0][i]);
+                    }
+            }
+        }
+        if(matriz[0][1] == 'S')
+            return "SI";
+    }
+    return "NO";
 }
 
 int main(){
     GIC* gic = new GIC();
     gic = create_GIC();
-    cout<<language_empty(gic)<<endl;
+    //cout<<language_empty(gic)<<endl;
+    char** matriz = insert_matriz(gic);
+    Queue<char>* queue = insert_queue(gic,matriz);
+    ForwardList<int>* quantity = insert_quantity(gic);
+    cout<<language_empty_queue(gic,matriz,queue, quantity);
     delete gic;
     return 0;
 }
